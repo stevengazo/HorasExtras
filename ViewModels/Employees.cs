e0b7101b0355b84cccc488ad5817bb83dd9bff99
Steven Gazo M <43178863+stevengazo@users.noreply.github.com>
2024-11-28 17:39:31 -0600

    binding context

--- ViewModels/Employees.cs
+++ ViewModels/Employees.cs
@@ -2,7 +2,32 @@ using System;
 
 namespace HorasExtras.ViewModels;
 
-public class Employees: INotifyPropertyChangedAbs
+public class Employees : INotifyPropertyChangedAbs
 {
+    public Command IAddEmployee
+    {
+        get
+        {
+            return new Command(async () => await AddEmployeeAsync());
+        }
+        private set
+        {
 
+        }
+    }
+
+    private async Task AddEmployeeAsync()
+    {
+        try
+        {
+            var EmployeeName = await Application.Current.MainPage.DisplayPromptAsync("Nuevo Empleado", "Indique el nombre", "OK", "Cancelar");
+            var EmployeeLastname1 = await Application.Current.MainPage.DisplayPromptAsync("Nuevo Empleado", "Indique el Primer Apellido", "OK", "Cancelar");
+            var EmployeeLastname2 = await Application.Current.MainPage.DisplayPromptAsync("Nuevo Empleado", "Indique el Segundo Apellido", "OK", "Cancelar");
+
+        }
+        catch (Exception ex)
+        {
+            await Application.Current.MainPage.DisplayAlert("Error", ex.Message, "Ok");
+        }
+    }
 }
